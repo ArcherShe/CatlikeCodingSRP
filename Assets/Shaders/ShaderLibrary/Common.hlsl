6f0223866ddd2c5a610a5c8e14903cd7fe68dc5d
Archer <sqL.onepiece@outlook.com>
2023-08-17 19:25:48 +0800

    test up

--- Assets/Shaders/ShaderLibrary/Common.hlsl
+++ Assets/Shaders/ShaderLibrary/Common.hlsl
@@ -1,7 +1,6 @@
 #ifndef CUSTOM_COMMON_INCLUDED
 #define CUSTOM_COMMON_INCLUDED
     #include "UnityInput.hlsl"
-    // #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 
     #define UNITY_MATRIX_M unity_ObjectToWorld
     #define UNITY_MATRIX_I_M unity_WorldToObject
